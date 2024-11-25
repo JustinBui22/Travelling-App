2a2f03cc8f232b1af4eb2040fb6e9e31195f6ab8
Justin Bui <buidominhquanhp1999@gmail.com>
2024-11-26 01:34:21 +1030

    Project Initialization

---
+++ src/test/java/com/example/travelingapp/TheProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
